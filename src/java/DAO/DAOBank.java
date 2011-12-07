a29e8e7ff2c4d87272b8ab31666595135a97c5e7
hikaruit15 <hikaruit15@gmail.com>
2011-12-07 02:46:14 +0000

    Create DAOBank.java

---
+++ src/java/DAO/DAOBank.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAO;
+
+import Entities.Bank;
+import Server.MySQLConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author DinhBao
+ */
+public class DAOBank {
+
+    public static void insertBank(Bank bank){
+        Connection conn = MySQLConnection.getConnection();
+        String sql = "INSERT INTO bank (bank_id, bank_name, bank_address)"
+                + "VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, bank.getBankId());
+            ps.setString(2, bank.getBankName());
+            ps.setString(3, bank.getBankAddress());
+            boolean rs = ps.execute();
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Get information Bank from Db
+     * @param bankId
+     * @return
+     */
+    public static Bank getBank(int bankId) {
+        Bank bank = null;
+        Connection conn = MySQLConnection.getConnection();
+        String sql = "SELECT * FROM bank WHERE bank_id = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, bankId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                bank = new Bank();
+                bank.setBankId(bankId);
+                bank.setBankName(rs.getString("bank_name"));
+                bank.setBankAddress(rs.getString("bank_address"));
+            }
+            return bank;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOCustomer.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+}

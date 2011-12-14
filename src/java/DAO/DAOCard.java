91cedd0fda4daf0861078e0c756ad87e0beaa5ff
hikaruit15 <hikaruit15@gmail.com>
2011-12-14 14:53:49 +0000

    Them vao updateCard

--- src/java/DAO/DAOCard.java
+++ src/java/DAO/DAOCard.java
@@ -67,9 +67,33 @@ public class DAOCard {
         } catch (SQLException ex) {
             Logger.getLogger(DAOCard.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 
+    /**
+     * Update lai thong tin card
+     * @param card
+     */
+    public static void updateCard(Card card){
+        Connection conn = MySQLConnection.getConnection();
+        String sql = "UPDATE card SET card_pin = ?,customer_id = ?,card_expiredate = ? WHERE card_id = ?";
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, card.getCardPin());
+            ps.setInt(2, card.getCustomerId());
+            ps.setDate(3, card.getCardExpiredate());
+            ps.setInt(4, card.getCardId());
+            ps.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOCard.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Ham kiem tra ngay su dung cua card
+     * @param cardId
+     * @return
+     */
     public static boolean checkExpireDay(int cardId){
         Connection conn = MySQLConnection.getConnection();
         String sql = "SELECT * FROM card WHERE card_id = ? and DATE(card_expiredate) >= DATE(NOW())";
@@ -87,8 +111,6 @@ public class DAOCard {
             Logger.getLogger(DAOCard.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
-
-
     }
     
 }
